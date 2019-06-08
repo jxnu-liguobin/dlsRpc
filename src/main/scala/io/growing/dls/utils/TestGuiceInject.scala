0fb7378d76732f9e6bbfab425561c83bda7370ee
梦境迷离 <568845948@qq.com>
2019-06-09 01:43:30 +0800

    Add [this] for consul client

--- src/main/scala/io/growing/dls/utils/TestGuiceInject.scala
+++ src/main/scala/io/growing/dls/utils/TestGuiceInject.scala
@@ -1,6 +1,5 @@
 package io.growing.dls.utils
 
-import com.typesafe.scalalogging.LazyLogging
 import io.growing.dls.centrel.discovery.RPCDiscoveryService
 import io.growing.dls.centrel.registry.RPCRegisterService
 import io.growing.dls.meta.ServiceAddress
@@ -11,7 +10,7 @@ import io.growing.dls.meta.ServiceAddress
  * @author 梦境迷离
  * @version 1.0, 2019-06-08
  */
-object TestGuiceInject extends App  {
+object TestGuiceInject extends App {
 
   private lazy val register: RPCRegisterService = ServiceLoadUtil.getProvider(classOf[RPCRegisterService])
   private lazy val discover: RPCDiscoveryService = ServiceLoadUtil.getProvider(classOf[RPCDiscoveryService])
