c4a3608f29e77ddbc0936c80a6bc2054293ca05b
梦境迷离 <568845948@qq.com>
2019-07-10 22:59:34 +0800

    Add interfaces for loadBalancer to get all IP -> weight and method for merging IP -> weight

--- dlsRpc-consul/src/main/scala/io/growing/dlsrpc/consul/loadbalancer/LoadBalancer.scala
+++ dlsRpc-consul/src/main/scala/io/growing/dlsrpc/consul/loadbalancer/LoadBalancer.scala
@@ -1,10 +1,15 @@
 package io.growing.dlsrpc.consul.loadbalancer
 
+import java.util.{Map => JMap}
+
+import io.growing.dlsrpc.common.metadata.ServiceAddress
+
+
 /**
  * 服务发现负载均衡顶级接口
  *
  * @author 梦境迷离
- * @version 1.0, 2019-06-08
+ * @version 1.1, 2019-06-08
  */
 trait LoadBalancer[T] {
 
@@ -16,11 +21,26 @@ trait LoadBalancer[T] {
   def next: T
 
   /**
-   * HASH IP + 权值 + 默认权值
+   * HASH Client IP + 权值 + 默认权值
    *
    * @param remoteIp
    * @return
    */
   def next(remoteIp: String): T = ???
+
+  /**
+   * 提供在轮询时更新[server,weight]的接口
+   *
+   * @param addMaps
+   * @return
+   */
+  def ++(addMaps: JMap[_ <: ServiceAddress, Int]): LoadBalancer[T] = ???
+
+  /**
+   * 获取实时的某负载均衡实例含有的所有[server,weight]
+   *
+   * @return
+   */
+  def getServiceAddressMap: JMap[_ <: ServiceAddress, Int] = ???
 
 }
--- dlsRpc-consul/src/main/scala/io/growing/dlsrpc/consul/loadbalancer/WeightLoadBalancer.scala
+++ dlsRpc-consul/src/main/scala/io/growing/dlsrpc/consul/loadbalancer/WeightLoadBalancer.scala
@@ -7,7 +7,7 @@ import java.util.{ArrayList => JArrayList, List => JList, Map => JMap}
 import com.google.common.collect.Maps
 import com.typesafe.scalalogging.LazyLogging
 import io.growing.dlsrpc.common.config.Configuration._
-import io.growing.dlsrpc.common.metadata.WeightServiceAddress
+import io.growing.dlsrpc.common.metadata.{ServiceAddress, WeightServiceAddress}
 import io.growing.dlsrpc.common.utils.CheckCondition
 import io.growing.dlsrpc.common.utils.ImplicitUtils._
 
@@ -17,7 +17,7 @@ import io.growing.dlsrpc.common.utils.ImplicitUtils._
  * 没有完全支持泛型，只负责IP轮询，暂无容错
  *
  * @author 梦境迷离
- * @version 1.0, 2019-06-14
+ * @version 1.1, 2019-06-14
  * @param weightServiceAddresses 所有存活的服务
  */
 class WeightLoadBalancer[T](val weightServiceAddresses: JList[T]) extends LoadBalancer[T]
@@ -27,7 +27,10 @@ class WeightLoadBalancer[T](val weightServiceAddresses: JList[T]) extends LoadBa
 
   //临时服务地址，可能被修改
   @volatile
-  protected[this] var serviceIps: JMap[WeightServiceAddress, Int] = _
+  protected[this] var serviceIps: WAMapType = _
+
+  @volatile
+  override def getServiceAddressMap: WAMapType = this.serviceIps
 
   if (weightServiceAddresses.isEmpty || weightServiceAddresses.size() == 0) {
     serviceIps = defaultWeightServiceAddress
@@ -38,32 +41,10 @@ class WeightLoadBalancer[T](val weightServiceAddresses: JList[T]) extends LoadBa
     logger.info("Number of merged is {}", serviceIps.size())
   }
 
-  /**
-   * 如果有传入，则合并默认。
-   * 传入的拥有默认优先级但是在conf中配置了ip的服务将得到更高优先级
-   *
-   * @param weightServiceAddresses
-   * @return
-   */
-  private[this] def convertToMap(weightServiceAddresses: JList[T]): JMap[WeightServiceAddress, Int] = {
-    //对传进来的进行放入map中，并对默认配置进行合并
-    val ms: JMap[WeightServiceAddress, Int] = Maps.newConcurrentMap()
-    for (service <- weightServiceAddresses.iterator()) {
-      val s: WeightServiceAddress = service.asInstanceOf[WeightServiceAddress]
-      ms.put(s, s.getWeight)
-    }
-    //以传入的存活的服务为准，若配置文件中也配置了，则会增加该服务的权值。注意 Key 需要实现 hashcode equals
-    //服务存活但是没有配置，会有低优先级的默认值
-    //存活并且配置了，权值会增加，服务端任务该服务地址是用户更想去取得的地址
-    val mergeMap: JMap[WeightServiceAddress, Int] = defaultWeightServiceAddress ++ ms.map(m =>
-      m._1 -> (m._2 + defaultWeightServiceAddress.getOrElse(m._1, weight)))
-    mergeMap
-  }
-
   override def next: T = {
     CheckCondition.conditionException(SERVICE_IP_LIST.size() == 0, "can't use default ip and set error in dlsRpc.conf")
     //给它设置相等的权值
-    val serverMap: JMap[WeightServiceAddress, Int] = serviceIps
+    val serverMap: WAMapType = serviceIps
     //獲取ip列表list
     val it: util.Iterator[WeightServiceAddress] = serverMap.keySet().iterator()
     val serverList = new JArrayList[WeightServiceAddress]
@@ -74,14 +55,18 @@ class WeightLoadBalancer[T](val weightServiceAddresses: JList[T]) extends LoadBa
         serverList.add(server)
       }
     }
-
     val pos = ThreadLocalRandom.current.nextInt(serverList.size())
     serverList.get(pos).asInstanceOf[T]
   }
 
+  override def ++(addMaps: JMap[_ <: ServiceAddress, Int]): LoadBalancer[T] = {
+    this.serviceIps = mergeMap(this.serviceIps, addMaps.asInstanceOf[WAMapType])
+    this
+  }
+
   override def next(remoteIp: String): T = {
     CheckCondition.conditionException(SERVICE_IP_LIST.size() == 0, "can't use default ip because param error in dlsRpc.conf")
-    val serverMap: JMap[WeightServiceAddress, Int] = serviceIps
+    val serverMap: WAMapType = serviceIps
     val it: util.Iterator[WeightServiceAddress] = serverMap.keySet().iterator()
     val serverList = new JArrayList[WeightServiceAddress]
     while (it.hasNext) {
@@ -96,6 +81,39 @@ class WeightLoadBalancer[T](val weightServiceAddresses: JList[T]) extends LoadBa
     val pos = hashCode % serverListSize
     serverList.get(pos).asInstanceOf[T]
   }
+
+  /**
+   * 如果有传入，则合并默认。
+   * 传入的拥有默认优先级但是在conf中配置了ip的服务将得到更高优先级
+   *
+   * @param weightServiceAddresses
+   * @return
+   */
+  private[this] def convertToMap(weightServiceAddresses: JList[T]): WAMapType = {
+    //对传进来的进行放入map中，并对默认配置进行合并
+    val ms: WAMapType = Maps.newConcurrentMap()
+    for (service <- weightServiceAddresses.iterator()) {
+      val s: WeightServiceAddress = service.asInstanceOf[WeightServiceAddress]
+      ms.put(s, s.getWeight)
+    }
+    //以传入的存活的服务为准，若配置文件中也配置了，则会增加该服务的权值。注意 Key 需要实现 hashcode equals
+    //服务存活但是没有配置，会有低优先级的默认值
+    //存活并且配置了，权值会增加，服务端任务该服务地址是用户更想去取得的地址
+    mergeMap(defaultWeightServiceAddress, ms)
+  }
+
+  /**
+   * 合并Map
+   *
+   * @param default 默认值
+   * @param target  目标增长值
+   * @return
+   */
+  private[this] def mergeMap(default: WAMapType, target: WAMapType) = {
+    val mergeMap: WAMapType = default ++ target.map(m =>
+      m._1 -> (m._2 + default.getOrElse(m._1, weight)))
+    mergeMap
+  }
 }
 
 //单独测试这个负载均衡时增加 extends App，到运行TestConsulService测试consul服务发现和注册时必须去掉，否则defaultWeightServiceAddress不会被初始化
@@ -108,6 +126,11 @@ class WeightLoadBalancer[T](val weightServiceAddresses: JList[T]) extends LoadBa
 //最后解决就是去掉被依赖对象的extends App 即时这个只是测试时用到的main方法但是最后跑整体代码时这个是必须要去掉，
 object WeightLoadBalancer {
 
+  //后续泛型尽可能使用父类传递，需要时转换为子类。
+  //注意，不能将子类转换为父类，里氏替换原则：任何基类可以出现的地方，子类一定可以出现。
+  //也就是子类是可以替代父类，反之不行（可能丢失属性）
+  type WAMapType = JMap[WeightServiceAddress, Int]
+
   //默认权值
   private val weight = 5
 
@@ -118,17 +141,21 @@ object WeightLoadBalancer {
   private val port = WEB_SERVER_PORT
 
   //默认的服务列表，从配置文件读取，表示启用的服务列表，隐式去重
-  private final lazy val defaultWeightServiceAddress: JMap[WeightServiceAddress, Int] = Maps.newConcurrentMap()
+  private final lazy val defaultWeightServiceAddress: WAMapType = Maps.newConcurrentMap()
   SERVICE_IP_LIST.forEach(x => defaultWeightServiceAddress.put(new WeightServiceAddress(x, port, high_weight), high_weight))
   SERVICE_IP_LIST.forEach(x => CheckCondition.conditionException(!x.toString.matches(IP_PATTERN), "not an valid format like ip"))
 
-  //Test data
+  //  Test data
   //  val s = new util.ArrayList[WeightServiceAddress]()
+  //  val s2 = new util.ArrayList[WeightServiceAddress]()
   //  s.add(new WeightServiceAddress("127.0.0.1", port, 2))
-  //  s.add(new WeightServiceAddress("127.0.1.5", port, 1))
-  //  s.add(new WeightServiceAddress("127.7.1.2", port, 1))
-  //  s.add(new WeightServiceAddress("127.1.1.2", port, 1))
-  //  s.add(new WeightServiceAddress("192.168.1.1", port, 1))
-  //  println(new WeightLoadBalancer(s).next)
-  //  println(new WeightLoadBalancer(s).next("127.0.5.1"))
+  //  s.add(new WeightServiceAddress("127.0.1.2", port, 1))
+  //  s.add(new WeightServiceAddress("127.0.1.3", port, 1))
+  //  s.add(new WeightServiceAddress("127.1.1.4", port, 1))
+  //  s2.add(new WeightServiceAddress("192.168.1.1", port, 1))
+  //  val wl = new WeightLoadBalancer(s)
+  //  val wl2 = new WeightLoadBalancer(s2)
+  //  val ret = wl2 ++ wl.getServiceAddressMap
+  //  println(ret.getServiceAddressMap.size())
+  //  println(ret.next)
 }
